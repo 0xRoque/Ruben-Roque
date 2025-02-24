0f5c0f9760f4f51362fadc8ce270cbc00eb304e4
0xroque <kodaksixx@gmail.com>
2025-02-24 18:31:39 +0000

    Mensagem explicativa sobre as alterações

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,15 +12,23 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="React" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="NextJs" />
         <SkillList src={checkMarkIcon} skill="Express" />
         <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
         <SkillList src={checkMarkIcon} skill="Bootstrap" />
+      </div>
+     < hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Axios" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
+        <SkillList src={checkMarkIcon} skill="Mongoose" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="GitHub" />
       </div>
     </section>
   );
